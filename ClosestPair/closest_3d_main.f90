3499d2e7da40c53d440625ebde75a931702d7830
Manmeet Singh <manmeet20singh11@gmail.com>
2016-09-04 16:27:52 +0530

    Tested and added closest pair algorithm, both brute force and divide and conquer

---
+++ ClosestPair/closest_3d_main.f90
@@ -0,0 +1,26 @@
+program TestClosestPair
+  use ClosestPair
+  implicit none
+ 
+  integer, parameter :: n = 10000
+ 
+  integer :: i
+  real :: x, y, z
+  type(point), dimension(n) :: points
+ 
+  type(point), dimension(2) :: p
+  real :: dp, dr
+ 
+  ! init the random generator here if needed
+ 
+  do i = 1, n
+     call random_number(x)
+     call random_number(y)
+     points(i) = point(x*20.0-10.0, y*20.0-10.0, z*20.0-10.0)
+  end do
+ 
+  call closest_pair_simple(points, p, dp)
+  print *, "sim ", p(1)%x,p(2)%x,p(1)%y,p(2)%y,p(1)%z,p(2)%z
+  call closest_pair(points, p, dr)
+  print *, "rec ", p(1)%x,p(2)%x,p(1)%y,p(2)%y,p(1)%z,p(2)%z
+end program TestClosestPair
