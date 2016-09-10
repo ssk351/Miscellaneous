473e9f4897221c4cb7a1133bbb2c8aa7032ab2ea
Manmeet Singh <manmeet20singh11@gmail.com>
2016-09-10 19:16:00 +0530

    Implemented normalization in the neural network 4 layered with 2 hidden layers

--- ISMR_NINO_AI_experiments/ismr_nino3.py
+++ ISMR_NINO_AI_experiments/ismr_nino3.py
@@ -1,8 +1,12 @@
 from csvread import readcsv
 from neuron3to4lyr import NN
+from norm import normalize
 
 ismr  = readcsv('ismr.csv')
 nino3 = readcsv('nino3.csv')
+ismr_norm = normalize(ismr)
+nino3_norm = normalize(nino3)
+
 no_of_months = len(ismr)
 
 ismr_nino3 = [ [ [None],[None] ] for x in xrange(no_of_months) ]
@@ -10,19 +14,40 @@ ismr_nino3_train = [ [ [None],[None] ] for x in xrange(no_of_months-500) ]
 ismr_nino3_test = [ [ [None],[None] ] for x in xrange(500) ]
 
 
-for i in range(no_of_months):
-	ismr_nino3[i][0][0] = ismr[i]
-	ismr_nino3[i][1][0] = nino3[i]
+#for i in range(no_of_months):
+#	ismr_nino3[i][0][0] = ismr[i]
+#	ismr_nino3[i][1][0] = nino3[i]
+#
+#ismr_nino3_train = ismr_nino3[0:no_of_months-500-1]
+#ismr_nino3_test  = ismr_nino3[no_of_months-500:no_of_months-1]
+## create a network with two input, two hidden, and one output nodes
+#n = NN(1, 4, 2 , 1)
+## train it with some patterns
+#n.train(ismr_nino3_train)
+## save a network
+## test it
+#n.test(ismr_nino3_test)
+#
+#
 
-ismr_nino3_train = ismr_nino3[0:no_of_months-500-1]
-ismr_nino3_test  = ismr_nino3[no_of_months-500:no_of_months-1]
+nino3_ismr = [ [ [None],[None] ] for x in xrange(no_of_months) ]
+nino3_ismr_train = [ [ [None],[None] ] for x in xrange(no_of_months-500) ]
+nino3_ismr_test = [ [ [None],[None] ] for x in xrange(500) ]
+
+
+for i in range(no_of_months):
+	nino3_ismr[i][0][0] = nino3_norm[i]
+	nino3_ismr[i][1][0] = ismr_norm[i]
+
+nino3_ismr_train = nino3_ismr[0:no_of_months-500-1]
+nino3_ismr_test  = nino3_ismr[no_of_months-500:no_of_months-1]
 # create a network with two input, two hidden, and one output nodes
-n = NN(1, 4, 2 , 1)
+n = NN(1, 2, 1 , 1)
 # train it with some patterns
-n.train(ismr_nino3_train)
+n.train(nino3_ismr_train)
 # save a network
 # test it
-n.test(ismr_nino3_test)
+n.test(nino3_ismr_test)
 
 #pat = [
 #        [[0,0], [0]],
